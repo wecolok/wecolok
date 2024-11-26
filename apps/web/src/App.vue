faa6b9874f9329b1cca5e4c29be2f39323456dea
Lapanxd <morgan.dumas@outlook.fr>
2024-11-26 23:06:16 +0100

    feat(web): add app components

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,12 +1,9 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { User } from "@repo/models/types";
-import AppButton from "./components/AppButton.vue";
 
 const user: User = {};
 </script>
 
-<template>
-  <app-button>COuicou</app-button>
-</template>
+<template></template>
 
 <style scoped></style>
