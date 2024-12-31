9fee1648d4cdaef63d6181f62419138186bae324
Lapanxd <81854332+Lapanxd@users.noreply.github.com>
2024-12-31 18:42:02 +0100

    chore(ci): add ci to deploy to VPS (#1)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,11 +2,27 @@ import { NestFactory, Reflector } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { JwtAuthGuard } from "./auth/guards/jwt-auth.guard";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle("WeColok API")
+    .setDescription("API documentation for WeColok")
+    .setVersion("1.0")
+    .addBearerAuth() // Optionnel : Ajoute un support pour les tokens JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  app.enableCors({
+    origin: ["http://147.79.100.203", "http://localhost:4200"],
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true,
+  });
 
   app.useGlobalGuards(new JwtAuthGuard(new Reflector()));
 
@@ -14,4 +30,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
